58509b335594bc6b03c8c9dd97e2a8f63ff88537
chenallee <lchenalloy@gmail.com>
2020-02-07 22:27:22 -0500

    basic write to file

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ const axios = require('axios');
 const fs = require('fs');
 
 const questions = require('./lib/questions');
+const formatMarkdown = require(`./lib/format-markdown`);
 
 //ask user questions
 inquirer
   .prompt(questions)
   .then(response => {
-    // axios
-    // .get() //get email & avatar
-    // .then(({data}) => {
-    //   //write to file
+    axios
+    .get(`https://api.github.com/users/${response.username}`) //get email & avatar
+    .then(({data}) => {
 
-    // }
-    console.log(response);
-    console.log(`done`);
+      const responseData = {...response, githubData: {...data}};
+      const markdownFormatted = formatMarkdown(responseData);
+
+      //write to file
+      fs.writeFile(`./${response.title}.md`, markdownFormatted, err =>{
+        if(err){
+          return console.log(err);
+        }
+        console.log(`Success!`);
+      });
+    });
   }
   );
 
---
+++ test.md
@@ -0,0 +1,24 @@
+
+  # test
+
+  undefined
+
+  undefined
+
+  undefined
+
+  undefined
+
+  ### License: 
+
+  Apache License 2.0
+
+  ### Contact: 
+
+  ![Avatar for chenallee on Github](https://avatars1.githubusercontent.com/u/22664384?v=4) 
+
+
+  Github: [chenallee](https://github.com/chenallee)
+  
+
+  
